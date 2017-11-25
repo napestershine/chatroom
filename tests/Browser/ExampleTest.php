bc513f13a8a03c047847e3c4782e5f01e82ce65f
Manoj <napestershine@gmail.com>
2017-11-25 12:24:57 +0530

    Add laravel dusk

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -2,12 +2,15 @@
 
 namespace Tests\Browser;
 
+use App\User;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ExampleTest extends DuskTestCase
 {
+    use DatabaseMigrations;
+
     /**
      * A basic browser test example.
      *
@@ -15,9 +18,28 @@ class ExampleTest extends DuskTestCase
      */
     public function testBasicExample()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee('Laravel');
+        $user1 = factory(User::class)->create([
+            'name' => 'Manoj'
+        ]);
+
+        $user2 = factory(User::class)->create([
+            'name' => 'Kri'
+        ]);
+
+        $this->browse(function ($first, $second) use ($user1, $user2) {
+            $first->loginAs($user1)
+                ->visit('/chat')
+                ->waitFor('.chat-composer');
+
+            $second->loginAs($user2)
+                ->visit('/chat')
+                ->waitFor('.chat-composer')
+                ->type('#message', 'Hey Manoj')
+                ->press('Send');
+
+            $first->waitForText('Hey Manoj')
+                ->assertSee('Hey Manoj');
         });
+
     }
 }
