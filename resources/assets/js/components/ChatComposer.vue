b986194a70637d2b927db9783c491998d74c2907
Manoj <napestershine@gmail.com>
2017-11-25 11:51:25 +0530

    updated component

--- resources/assets/js/components/ChatComposer.vue
+++ resources/assets/js/components/ChatComposer.vue
@@ -1,6 +1,6 @@
 <template lang="html">
     <div class="chat-composer">
-   <input type="text" placeholder="Start typing your message..." v-model="messageText" @keydown.enter="sendMessage">
+   <input type="text" placeholder="Start typing your message..." v-model="messageText" @keyup.enter="sendMessage">
    <button class="btn btn-primary" @click="sendMessage">Send</button>
 </div>
 </template>
